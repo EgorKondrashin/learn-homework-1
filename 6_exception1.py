e1660f0ac2c4e1cfd13f033f8939dc24d2fdb3c6
Egor Kondrashin <kondrashin-egor@mail.ru>
2024-05-22 19:59:54 +0500

    Completed the task 6

--- 6_exception1.py
+++ 6_exception1.py
@@ -10,11 +10,18 @@
     
 """
 
+
 def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    while True:
+        try:
+            input('Как дела?\n')
+        except KeyboardInterrupt:
+            print('Пока!')
+            break
+
+
 if __name__ == "__main__":
     hello_user()
