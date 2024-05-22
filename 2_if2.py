c86f0209571a919802a5d971e69ae0b73f5fd1ca
Egor Kondrashin <kondrashin-egor@mail.ru>
2024-05-22 18:31:51 +0500

    Completed the task 2_if2

--- 2_if2.py
+++ 2_if2.py
@@ -15,12 +15,32 @@
 
 """
 
+
+def check_string(str1, str2):
+    if type(str1) is str and type(str2) is str:
+        if str1 == str2:
+            return 1
+        elif str2 == 'learn':
+            return 3
+        elif len(str1) > len(str2):
+            return 2
+    else:
+        return 0
+
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    print(check_string('1', '1'))  # 1
+    print(check_string(1, '1'))  # 0
+    print(check_string(1, 1))  # 0
+    print(check_string('привет тебе', 'привет'))  # 2
+    print(check_string('Python', 'learn'))  # 3
+    print(check_string('1', '2'))  # None
+    print(check_string('привет', 'привет тебе'))  # None
+
+
 if __name__ == "__main__":
     main()
